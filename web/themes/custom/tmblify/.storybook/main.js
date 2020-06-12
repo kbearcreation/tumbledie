b7117d3ede06a6897ae3eab8b34496cd484aeca6
Kevin Baringer <kevin@kbearcreation.com>
2020-06-12 17:21:45 -0400

    Modifications to tmblify theme for Storybook.js usage.

---
+++ web/themes/custom/tmblify/.storybook/main.js
@@ -0,0 +1,13 @@
+/*
+ * Addons added to this installation of Storybook
+ * https://github.com/storybookjs/storybook/tree/master/addons/storysource
+ * https://github.com/storybookjs/storybook/tree/master/addons/viewport
+ *
+ */
+
+module.exports = {
+  addons: [
+    '@storybook/addon-storysource',
+    '@storybook/addon-viewport/register',
+  ],
+};
