3a9804e59e915fa188d22f2a531cda20cf72dc89
Kevin Baringer <kevin@kbearcreation.com>
2020-08-19 23:11:48 -0400

    Reset forms.stories.js to remove webform settings for now (caused ESLint error). Removed max-width value from textfields, which was being applied to all form fields.

--- web/themes/custom/tmblify/components/01-atoms/forms/forms.stories.js
+++ web/themes/custom/tmblify/components/01-atoms/forms/forms.stories.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
 import checkbox from './checkbox/checkbox.twig';
-import webformcheckbox from './webform-checkbox/webform-checkbox.twig';
 import radio from './radio/radio.twig';
 import select from './select/select.twig';
 import textfields from './textfields/textfields.twig';
 
 import checkboxData from './checkbox/checkbox.yml';
-import webformcheckboxData from './webform-checkbox/webform-checkbox.yml';
 import radioData from './radio/radio.yml';
 import selectOptionsData from './select/select.yml';
 
@@ -18,11 +16,6 @@ export default { title: 'Atoms/Forms' };
 
 export const checkboxes = () => (
   <div dangerouslySetInnerHTML={{ __html: checkbox(checkboxData) }} />
-);
-export const webformCheckboxes = () => (
-  <div
-    dangerouslySetInnerHTML={{ __html: webformcheckbox(webformcheckboxData) }}
-  />
 );
 export const radioButtons = () => (
   <div dangerouslySetInnerHTML={{ __html: radio(radioData) }} />
